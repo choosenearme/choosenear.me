de2f73ec727f01ceae3b36b8321a08f34162341c
Paul Fryzel <paul@loosecubes.com>
2012-07-10 13:12:16 -0400

    Add styling and donation defaults to mobile

--- static/proposals/js/index.js
+++ static/proposals/js/index.js
@@ -6,11 +6,17 @@ $(document).ready( function() {
     var proposal = project.proposals[0];
     var secureImageURL = proposal.imageURL.replace("http://cdn.donorschoose.net/",
                                                    "https://choosenear.me/cdn/");
+    var donationBaseURL = "https://secure.donorschoose.org/donors/givingCart.html?proposalid=";
+    var donationAmount = "&donationAmount=5";
+    var donationOptions = "&utm_source=api&utm_medium=feed&utm_content=fundlink&utm_campaign=f8h932pdcukv";
+    var donationURL = donationBaseURL + proposal.id + donationAmount + donationOptions;
+
     $("img").attr({
       src: secureImageURL
     });
     $("h2").text(proposal.title);
     $("p").text(proposal.shortDescription);
+    $(".webview-button").attr('href', donationURL);
   });
 
   function getProposalId(){
